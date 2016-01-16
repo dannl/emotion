35e224b7f1939d5223950a0e806254e31e65245f
dannl <kevin.danliu@gmail.com>
2016-01-16 18:36:11 +0800

    add dlt site.

---
+++ lottery/src/main/java/com/qqdd/lottery/data/Constants.java
@@ -0,0 +1,10 @@
+package com.qqdd.lottery.data;
+
+/**
+ * Created by danliu on 2016/1/16.
+ */
+public class Constants {
+
+    public static final String DLT_REPO_URL_FORMAT = "http://www.lottery.gov.cn/lottery/dlt/History.aspx?p=28";
+
+}
