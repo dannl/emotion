776d83ce0cbdd5cdaae78372e4530a095231708b
dannl <kevin.danliu@gmail.com>
2016-01-20 22:06:05 +0800

    temp commit.

---
+++ lottery/src/main/java/com/qqdd/lottery/ui/view/FixHeightRecyclerView.java
@@ -0,0 +1,23 @@
+package com.qqdd.lottery.ui.view;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+
+/**
+ * Created by danliu on 2016/1/20.
+ */
+public class FixHeightRecyclerView extends RecyclerView {
+
+    public FixHeightRecyclerView(Context context) {
+        super(context);
+    }
+
+    public FixHeightRecyclerView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public FixHeightRecyclerView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
